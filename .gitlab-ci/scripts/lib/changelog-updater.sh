a4d192e815cdec9280a796f0b8a1da68c69ba555
Daniel Del Rio <daniel@passbolt.com>
2023-03-03 14:31:52 +0000

    PB-19300: use changelog updater as docker image instead as a cli

--- .gitlab-ci/scripts/lib/changelog-updater.sh
+++ .gitlab-ci/scripts/lib/changelog-updater.sh
@@ -1,23 +1,31 @@
-
-function install_changelog_updater() {
-  local url="${1}"
-  local version="${2:-latest}"
-  go install "$url@$version"
+function pull_docker_updater_image() {
+  registry="${1}"
+  tag="${2}"
+  docker pull "$registry:$tag"
+  docker tag "$registry:$tag" changelog-updater
 }
-
-function set_gitlab_private_repository() {
-  local url="${1}"
-  go env -w GOPRIVATE="$url"
+function run_changelog_updater() {
+  docker run changelog-updater "${@}"
 }
 
 function update_packages_changelog_and_tag() {
-  local config_file=$1
-  local passbolt_version=$2
-  local passbolt_flavour=$3
-  local filter=$4
+  local passbolt_version=$1
+  local passbolt_flavour=$2
+  local filter=$3
+  local source_changelog=$4
+  local debian_changelog=$5
+  local rpm_changelog=$6
+  local author_name=$7
+  local author_email=$8
+
   if ! is_release_candidate; then
     # TODO: check the right syntax when the command is ready
-    changelog_updater -c "$config_file" > /dev/null
+    run_changelog_updater "--source-changelog=$source_changelog" \
+                          "--debian-changelog=$debian_changelog" \
+                          "--rpm-changelog=$rpm_changelog" \
+                          "--passbolt-flavour=$passbolt_flavour" \
+                          "--name=$author_name" \
+                          "--email=$author_email"
     create_git_tag "$passbolt_version" "$passbolt_flavour" "$filter" stable
     return 0
   fi 
