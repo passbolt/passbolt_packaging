d2237c4e6246c9be4b336cb05c7a940d9a33aec5
Diego Lendoiro <diego@passbolt.com>
2023-03-10 09:32:50 +0100

    PB-23708: set-env returns component too

--- .gitlab-ci/scripts/lib/set-env.sh
+++ .gitlab-ci/scripts/lib/set-env.sh
@@ -1,9 +1,18 @@
+# This function parses a tag in the form of:
+# v3.11.0-rc.1-pro-all
+#
+# All of the fields are mandatory:
+# Version: v3.11.0-rc.1|v3.11.0
+# Passbolt flavour: pro|ce
+# Per package filter: all|rpm|debian
+#
+# It also provides the component based on if it is RC: testing|stable
 function parse_tag() {
   local tag=$1
 
   if is_release_candidate "$tag"; then
-    echo "$tag" | awk -F '-' '{print $1"-"$2,$3,$4}'
+    echo "$tag" | awk -F '-' '{print $1"-"$2,$3,$4,"testing"}'
   else
-    echo "$tag" | awk -F '-' '{print $1,$2,$3}'
+    echo "$tag" | awk -F '-' '{print $1,$2,$3,"stable"}'
   fi
 }
--- .gitlab-ci/scripts/spec/set_env_spec.sh
+++ .gitlab-ci/scripts/spec/set_env_spec.sh
@@ -1,15 +1,15 @@
 Describe "parse_tag function"
-  
+
 
   Include "lib/version-check.sh"
   Include "lib/set-env.sh"
   It "parses the correct version flavour and filter from a non release candidate tag"
     When call parse_tag "v3.11.0-ce-all"
-    The output should equal "v3.11.0 ce all"
+    The output should equal "v3.11.0 ce all stable"
   End
   It "parses the correct version flavour and filter from a release candidate tag"
     When call parse_tag "v3.11.0-rc.2-pro-rpm"
-    The output should equal "v3.11.0-rc.2 pro rpm"
+    The output should equal "v3.11.0-rc.2 pro rpm testing"
   End
 
 End
@@ -25,17 +25,17 @@ Describe "parsed tag array"
 
   Include "lib/version-check.sh"
   Include "lib/set-env.sh"
-  It "containse a non release candidate version flavour and filter in the right order"
+  It "contains a non release candidate version flavour and filter in the right order"
     When call get_parsed_tag_array "v3.11.0-ce-all"
-    The output should equal "v3.11.0 ce all"
+    The output should equal "v3.11.0 ce all stable"
   End
-  It "containse a release candidate version flavour and filter in the right order"
+  It "contains a release candidate version flavour and filter in the right order"
     When call get_parsed_tag_array "v3.11.0-rc.2-pro-rpm"
-    The output should equal "v3.11.0-rc.2 pro rpm"
+    The output should equal "v3.11.0-rc.2 pro rpm testing"
   End
-  It "containse a release candidate version flavour and NO filter in the right order"
+  It "contains a release candidate version flavour and NO filter in the right order"
     When call get_parsed_tag_array "v3.11.0-rc.2-pro"
-    The output should equal "v3.11.0-rc.2 pro"
+    The output should equal "v3.11.0-rc.2 pro testing"
   End
 
 End
