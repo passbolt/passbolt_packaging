33008102f7e89f5ff1af499f2c349da6ebbbea5d
Diego Lendoiro <diego@passbolt.com>
2023-03-13 13:39:31 +0100

    PB-23708: do not check version.php on RC

--- .gitlab-ci/scripts/lib/version-check.sh
+++ .gitlab-ci/scripts/lib/version-check.sh
@@ -4,16 +4,6 @@ function is_valid_api_tag () {
     return 1
   fi
 }
- 
-function validate_config_version_and_api_tag () {
-  local version_file="$1"
-  local version 
-  version=$(echo "$PASSBOLT_VERSION" | tr -d 'v')
-  if ! grep -q "$version" "$version_file"; then
-    echo "Version number in version.php does not match the tag: $PASSBOLT_VERSION"
-    return 1
-  fi
-}
 
 function is_release_candidate () {
   local version=$1
@@ -22,3 +12,20 @@ function is_release_candidate () {
   fi
   return 0
 }
+
+function validate_config_version_and_api_tag () {
+  local version_file="$1"
+  local version
+  version=$(echo "$PASSBOLT_VERSION" | tr -d 'v')
+
+  if is_release_candidate "$PASSBOLT_VERSION"; then
+    echo "We don't validate versions on RC"
+    return 0
+  fi
+
+  if ! grep -q "$version" "$version_file"; then
+    echo "Version number in version.php does not match the tag: $PASSBOLT_VERSION"
+    return 1
+  fi
+}
+
