96e5a2a38dd3094449e28c24683e671de5572557
Diego Lendoiro <diego@passbolt.com>
2025-04-07 13:19:14 +0000

    feat(packaging)!: PB-41373 set php8.2 as the minimal php version and removed unsupported distributions

--- tests/fixtures/setup-php-rhel.sh
+++ tests/fixtures/setup-php-rhel.sh
@@ -6,26 +6,13 @@ set -eu
 
 OS_VERSION=$(grep -E '^VERSION_ID=' /etc/os-release | awk -F= '{print $2}' | sed 's/\"//g')
 OS_VERSION_MAJOR=$(echo ${OS_VERSION:0:1} | bc)
+PHP_VERSION=8.2
 
-if [ ${OS_VERSION_MAJOR} -eq 8 ]
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+if ! rpm -qa | grep -q remi-release
 then
-    dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
-    if ! rpm -qa | grep -q remi-release
-    then
-      yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-    fi
-    dnf module reset php -y
-    dnf module install php:remi-8.1 -y
-elif [ ${OS_VERSION_MAJOR} -eq 9 ]
-then
-    dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
-    if ! rpm -qa | grep -q remi-release
-    then
-      dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
-    fi
-    dnf module reset php -y
-    dnf module enable php:remi-8.1 -y
-    dnf install php -y
-else
-  exit 1
+  dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
 fi
+dnf module reset php -y
+dnf module enable php:remi-$PHP_VERSION -y
+dnf install php -y
