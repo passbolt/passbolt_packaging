fc1abaf7874976dea8ecd3ad47d169668cbc4119
Louis Vallat <louis@passbolt.com>
2026-07-10 14:39:25 +0200

    feat: PB-52915: simplified EPEL setup script for testing RedHat based distributions
    Signed-off-by: Louis Vallat <louis@passbolt.com>

--- tests/fixtures/setup-epel.sh
+++ tests/fixtures/setup-epel.sh
@@ -8,12 +8,4 @@ set -eu
 OS_VERSION=$(grep -E '^VERSION_ID=' /etc/os-release | awk -F= '{print $2}' | sed 's/\"//g')
 OS_VERSION_MAJOR=$(echo ${OS_VERSION:0:1} | bc)
 
-if [ "${OS_VERSION_MAJOR}" -eq 8 ]
-then
-    dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
-elif [ "${OS_VERSION_MAJOR}" -eq 9 ]
-then
-    dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
-else
-  exit 1
-fi
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-"${OS_VERSION_MAJOR}".noarch.rpm -y
